0008b8d8b574c0aa3486b0acc114bd4b656cdcbe
yooneerum <yooneerum@gmail.com>
2025-11-14 11:13:33 +0900

    feat: db연결중

--- src/main/java/org/yoon/planner/DBConnection.java
+++ src/main/java/org/yoon/planner/DBConnection.java
@@ -1,9 +1,31 @@
 package org.yoon.planner;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBConnection {
+
+    private static Connection conn = null;
     public static Connection getConnection() {
-        return null;
+        if(conn == null) {
+            try {
+                Class.forName("org.sqlite.JDBC");
+                conn = DriverManager.getConnection("jdbc:sqlite:mywordlist.db");
+            } catch (ClassNotFoundException | SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return conn;
+    }
+
+    public static void closeConnection() {
+        if(conn != null) {
+            try {
+                conn.close();
+            }catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
